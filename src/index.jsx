ed32b4218faf83ab501c90e509793955ffbfe60d
Donald Whitely <dswhitely1@gmail.com>
2020-04-24 21:35:10 -0400

    React App Rendering With Store and Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+// Store
+import {Provider} from 'react-redux';
+import store from './store';
+
+// Router
+import {BrowserRouter as Router} from "react-router-dom";
+
 import App from './components/App';
 
-render(<App />, document.getElementById('root'));
+const EnchantedApp = () => (
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <App/>
+            </Router>
+        </Provider>
+    </React.StrictMode>
+);
+
+console.log(EnchantedApp);
+
+render(<EnchantedApp/>, document.getElementById('root'));
