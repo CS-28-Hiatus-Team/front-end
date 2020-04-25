db63119790519bca8b254ea24dc4ce3fe2da3e12
Donald Whitely <dswhitely1@gmail.com>
2020-04-24 21:57:16 -0400

    UseForm Hook Defined

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+
+export const useForm = (initialState, cb) => {
+    const [values, setValues] = useState(initialState);
+
+    const handleChange = ({target: {name, value}}) => setValues({...values, [name]: value});
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        cb();
+    };
+
+    const handleReset = () => setValues(initialState);
+
+    return [values, handleChange, handleSubmit, handleReset]
+};
