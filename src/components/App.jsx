b2d19e9021e1aed3c7b0c0226e25db19e8f55abc
Brianna Workman <brianna.workman@hotmail.com>
2020-04-27 15:10:18 -0400

    styles login and register pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'
-import Chat from "./chat/Chat";
-import {useActions} from "../store/useActions";
-import {ActionsProvider} from "../contexts/ActionsContext";
-import Navigation from "./Navigation/Navigation";
-import Auth from "./auth/Auth";
+import { Switch, Route } from 'react-router-dom';
+import Chat from './chat/Chat';
+import { useActions } from '../store/useActions';
+import { ActionsProvider } from '../contexts/ActionsContext';
+import Navigation from './Navigation/Navigation';
+import Auth from './auth/Auth';
 
 function App() {
-    const actions = useActions();
-    return (
-        <ActionsProvider value={actions}>
-            <Navigation/>
-            <Switch>
-                <Route path='/login' component={Auth} />
-                <Route path='/register' component={Auth} />
-            </Switch>
-            <div className="nes-container with-title is-centered">
-                <p className="title">Hello World</p>
-                <p>Why Hello There. A MUD is coming soon</p>
-            </div>
-            <Chat/>
-        </ActionsProvider>
-    )
+  const actions = useActions();
+  return (
+    <ActionsProvider value={actions}>
+      <Navigation />
+      <Switch>
+        <Route path='/login' component={Auth} />
+        <Route path='/register' component={Auth} />
+      </Switch>
+      <div className='nes-container with-title is-centered'>
+        <p className='title'>Hello World</p>
+        <p>Why Hello There. A MUD is coming soon</p>
+      </div>
+      <Chat />
+    </ActionsProvider>
+  );
 }
 
 export default App;
