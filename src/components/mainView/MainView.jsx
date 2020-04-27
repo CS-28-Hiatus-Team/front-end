2dd4481788baab25b5a4cee3ad22418dc33f7df5
wjonke <wesjonke@gmail.com>
2020-04-27 15:16:12 -0400

    Initial set up, added deps, abstracted chat into main view "home" route added expected components Minimap and Mainmap, still need built

---
+++ src/components/mainView/MainView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Chat from "../chat/Chat";
+
+function MainView() {
+  return (
+    <div>
+      {/* <MiniMap /> */}
+      <Chat />
+      {/* <MainMap /> */}
+    </div>
+  );
+}
+
+export default MainView;
