6426f1ae38490c4d52ae73a742ce8823dad89301
Donald Whitely <dswhitely1@gmail.com>
2020-04-29 06:08:16 -0400

    Login Store Hooked Up

--- src/store/auth/actions/useLogin.jsx
+++ src/store/auth/actions/useLogin.jsx
@@ -8,9 +8,8 @@ export const useLogin = () => {
 
     const login = useCallback(credentials => {
         dispatch({type: authTypes.AUTH_LOGIN_START});
-        axios().post('/api/login', credentials).then(res => {
-            console.log(res.data);
-            dispatch({type: authTypes.AUTH_LOGIN_SUCCESS, payload: res.data.key})
+        axios().post('/api/api-token-auth/', credentials).then(res => {
+            dispatch({type: authTypes.AUTH_LOGIN_SUCCESS, payload: res.data.token})
         }).catch(err => dispatch({type: authTypes.AUTH_LOGIN_FAILURE, payload: err.response}))
     }, [dispatch]);
 
