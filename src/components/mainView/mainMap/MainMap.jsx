694addc06fbc9ebdf6f7fee4c58b7e73dd2020ec
wjonke <wesjonke@gmail.com>
2020-04-30 16:42:05 -0400

    getting even to pull down from master

--- src/components/mainView/mainMap/MainMap.jsx
+++ src/components/mainView/mainMap/MainMap.jsx
@@ -6,18 +6,18 @@ import GameInfo from "../mainMap/features/GameInfo/index";
 import { tiles } from "./data/maps/1";
 import store from "./config/store";
 
-const styles = {
-  container: {
-    fontFamily: "Press Start 2P",
-    height: "100vh",
-    display: "block",
-    transition: "0.5s",
-    background:
-      "linear-gradient(rgba(0, 0, 0, 0.80), rgba(0, 0, 0, 0.95)), url(https://images.unsplash.com/photo-1440581572325-0bea30075d9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80)",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-  },
-};
+// const styles = {
+//   container: {
+//     fontFamily: "Press Start 2P",
+//     height: "100vh",
+//     display: "block",
+//     transition: "0.5s",
+//     background:
+//       "linear-gradient(rgba(0, 0, 0, 0.80), rgba(0, 0, 0, 0.95)), url(https://images.unsplash.com/photo-1440581572325-0bea30075d9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80)",
+//     backgroundRepeat: "no-repeat",
+//     backgroundSize: "cover",
+//   },
+// };
 
 const MainMap = (props) => {
   return (
@@ -33,7 +33,7 @@ const MainMap = (props) => {
       >
         <Map />
         <Player />
-        <GameInfo />
+        {/* <GameInfo /> */}
       </div>
     </container>
   );
