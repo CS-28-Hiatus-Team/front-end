0f3cd0ff0a45ea4b96a861444e22368777bab4fc
Brianna Workman <brianna.workman@hotmail.com>
2020-04-29 21:06:48 -0400

    adds ability to move between rooms

--- src/components/mainView/mainMap/MainMap.jsx
+++ src/components/mainView/mainMap/MainMap.jsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useEffect, useContext } from 'react';
+import { useSelector } from 'react-redux';
+import { ActionsContext } from '../../../contexts/ActionsContext';
 
 function MainMap() {
+  const { token } = useSelector((state) => state.auth);
+  const actions = useContext(ActionsContext);
+
+  function move(direction) {
+    actions.game.move(token, direction);
+    console.log(direction);
+  }
+
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+
+    switch (e.keyCode) {
+      case 37:
+        return 'w';
+
+      case 38:
+        return 'n';
+
+      case 39:
+        return 'e';
+
+      case 40:
+        return 's';
+
+      default:
+        console.log(e.keyCode);
+    }
+  };
+
+  window.addEventListener('keydown', (e) => {
+    move(handleKeyDown(e));
+  });
+
   return (
     <>
       <section>
